3949813e0801b0bd8e7c9585d84c9e2b196234f3
mikolajwoj <berkhovv@gmail.com>
2024-01-06 22:12:28 +0100

    Add new conditions to functions

--- NewtonsMethod.go
+++ NewtonsMethod.go
@@ -2,18 +2,24 @@ package main
 
 import (
 	"fmt"
+	"math"
 )
 
 func Sqrt(x float64) float64 {
 	z := float64(1)
-	fmt.Printf("Calculating square number of given %g", x)
+	fmt.Printf("Calculating square number of given %g\n", x)
+
 	//First use of z
-	z -= (z*z - x) / (2 * x)
+	//z -= (z*z - x) / (2 * x)
 
 	for i := 1; i < 10; i++ {
-		z -= (z*z - x) / (2 * x)
+		z -= (z*z - x) / (2 * z)
 		fmt.Printf("Attempt number: %v, number received:%g\n", i, z)
+		if math.Abs(math.Sqrt(x)-z) < 1e-9 {
+			break
+		}
 	}
+
 	return z
 }
 
